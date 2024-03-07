a277b044c64f9e51bef5e686ab64645a7b14429b
autoscorerx <158697009+autoscorerx@users.noreply.github.com>
2024-03-07 08:39:39 +0530

    Added additional hyperparam

--- trainYOLO.py
+++ trainYOLO.py
@@ -3,6 +3,8 @@ from ultralytics import YOLO
 data = None
 epochs = None
 imgsz = 640
+projectdirScratch = None
+projectdirPretrained = None
 
 # Create a new YOLO model from scratch
 modelScratch = YOLO('yolov8n.yaml')
@@ -12,14 +14,17 @@ modelPretrained = YOLO('yolov8n.pt')
 
 # Training the model from scratch
 # Train the model using the custom dataset for 300 epochs
-resultScratch = modelScratch.train(data=data, epochs=epochs, imgsz=imgsz, plots=True, task=detect)
+resultScratch = modelScratch.train(data=data, epochs=epochs, imgsz=imgsz, plots=True, task=detect, batch=16, patience=50, cache=True, device=0, project=projectdirScratch, dropout=0.2)
 
 # Evaluate the model's performance on the validation set
 resultSVal = modelScratch.val()
 
+print("Modedl training from scratch completed")
+key = input("Enter any key to continue")
+
 # Training the model from scratch
 # Train the model using the custom dataset for 300 epochs
-resultPretrained = modelPretrained.train(data=data, epochs=epochs, imgsz=imgsz, plots=True, task=detect)
+resultPretrained = modelPretrained.train(data=data, epochs=epochs, imgsz=imgsz, plots=True, task=detect, batch=16, patience=50, cache=True, device=0, project=projectdirPretrained, dropout=0.2)
 
 # Evaluate the model's performance on the validation set
 resultPVal = modelPretrained.val()
